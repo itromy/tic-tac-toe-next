add234d2dbdd757e1cdd8ea53a8380ec308ad7fa
Romy Pohl <romy.pohl.rp@gmail.com>
2025-01-23 11:27:58 +0100

    chore(): add workflow for deploy code to git hub pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: 'tic-tac-toe-next',
+  assetPrefix: '/tic-tac-toe-next/',
   reactStrictMode: true,
 };
 
